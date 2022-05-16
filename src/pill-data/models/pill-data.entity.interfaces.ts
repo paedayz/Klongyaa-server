708be68dfaaa49f361b22815825aa09ba5b5b3c9
paedayz <pae16phasit@gmail.com>
2022-05-16 11:57:19 +0700

    complete pill-data entity interfaces

--- src/pill-data/models/pill-data.entity.interfaces.ts
+++ src/pill-data/models/pill-data.entity.interfaces.ts
@@ -10,3 +10,26 @@ export interface ISaveTakeTime {
     cid: string;
     time: string;
 }
+
+export interface ISaveRealPill {
+    pillName: string;
+    properties: string;
+    effect: string;
+}
+
+export interface ISaveDangerPill {
+    rid: string;
+    pillName: string;
+    reason: string;
+}
+
+export interface ISaveCidRid {
+    cid: string;
+    rid: string;
+}
+
+export interface ISaveHistory {
+    cid: string;
+    uid: string;
+    task: string;
+}
